5a3057d486705175e6b8d30127842c337f3be1e6
Andrii-25 <kotsupyr.a@gmail.com>
2021-11-27 18:37:43 +0200

    add Player controller

---
+++ src/main/java/com/andrii/footballmanager/controller/PlayerController.java
@@ -0,0 +1,64 @@
+package com.andrii.footballmanager.controller;
+
+import com.andrii.footballmanager.entity.Player;
+import com.andrii.footballmanager.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/players")
+public class PlayerController {
+
+    @Autowired
+    private PlayerService playerService;
+
+    @PostMapping
+    public ResponseEntity<Player> create(@RequestBody Player player) {
+        try {
+            return ResponseEntity.ok(playerService.create(player));
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong...", e);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<Set<Player>> getAll() {
+        try {
+            return ResponseEntity.ok(playerService.getAll());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong...", e);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<Player> getById(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(playerService.getById(id));
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong...", e);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<Player> update(@PathVariable Long id, @RequestBody Player player) {
+        try {
+            return ResponseEntity.ok(playerService.update(id, player));
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong...", e);
+        }
+    }
+
+    @DeleteMapping
+    public void delete(@PathVariable Long id) {
+        try {
+            playerService.delete(id);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong...", e);
+        }
+    }
+}
